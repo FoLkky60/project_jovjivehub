5361a39d8c1565c720d41cf4645f8df1bc5a245c
Checkmartyr <p.tranf@gmail.com>
2024-05-19 17:55:43 +0700

    .

--- backend/controllers/getUserdataByID.js
+++ backend/controllers/getUserdataByID.js
@@ -7,31 +7,9 @@ module.exports = (req, res) => {
   User.findOne({ _id: UID })
     .then((user) => {
       if (user) {
-        //   bcrypt
-        //     .compare(password, user.password)
-        //     .then((match) => {
-        //       if (match) {
-        //         // res.setHeader("Access-Control-Allow-Origin", "https://localhost:5173");
-        //         res.status(200).json({
-        //           message: "Logged in successfully",
-        //           userDate: user._id,
-        //         });
-        //         // res.cookie("myCookie", "cookieValue", {
-        //         //   maxAge: 9000,
-        //         //   httpOnly: true,
-        //         // });
-        //       } else {
-        //         res.status(401).json({ message: "Incorrect password" });
-        //       }
-        //     })
-        //     .catch((err) => {
-        //       console.error("Error comparing passwords:", err);
-        //       res.status(500).json({ message: "Internal server error" });
-        //     });
         res.status(200).json({
           userDate: user,
         });
-        
       } else {
         res.status(404).json({ message: "User with this email not found" });
       }
